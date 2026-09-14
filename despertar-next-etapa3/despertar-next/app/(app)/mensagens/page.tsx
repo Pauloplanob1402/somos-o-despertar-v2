0f782e1b7cdff30a373f01628016910adb027104
Pauloplanob1402 <planob1402@gmail.com>
2026-09-14 17:40:56 -0300

    Delete despertar-next-etapa3/despertar-next directory

--- despertar-next-etapa3/despertar-next/app/(app)/mensagens/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ConversationsView } from "@/components/Mensagens";
-
-export default function MensagensPage() {
-  return (
-    <section className="view" style={{ padding: 0 }}>
-      <ConversationsView />
-    </section>
-  );
-}
